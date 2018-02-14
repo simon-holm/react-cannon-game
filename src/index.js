d4b4111d06899c312757895aa0fd0d1b58891686
spixooze <s_simon_s@hotmail.com>
2018-02-14 19:42:55 +0100

    Game Boiler up! redux setup

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { createElement } from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import './index.css'
+import Game from './containers/Game'
+import reducer from './reducers'
+import registerServiceWorker from './registerServiceWorker'
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Game />
+  </Provider>,
+  document.getElementById('root')
+)
+registerServiceWorker()
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+  message: 'REACT REDUX SVG GAME'
+}
+
+function reducer(state = initialState) {
+  return state
+}
+
+export default reducer
